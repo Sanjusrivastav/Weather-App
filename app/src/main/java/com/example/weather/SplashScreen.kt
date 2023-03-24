523eb2ebb2149127197f5b76d920e4014b7cc4fc
Sanjana Srivastav <sanjanasrivstav1997@gmail.com>
2023-03-25 01:18:09 +0530

    user permission

--- app/src/main/java/com/example/weather/SplashScreen.kt
+++ app/src/main/java/com/example/weather/SplashScreen.kt
@@ -1,41 +1,35 @@
 package com.example.weather
 
-import android.Manifest
+
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
-import android.location.LocationRequest
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
-class SplashScreen : AppCompatActivity() {
+
+class SplashScreen() : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private var myRequestCode =1022
+    private var myRequestCode = 1090
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            var intent = Intent(this,MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//
-//        },2000)
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         getLastLocation()
 
     }
+
     // Location permission -->deny
     // Location denied through setting
     //Location permission le lo
@@ -43,67 +37,94 @@ class SplashScreen : AppCompatActivity() {
 
 
     private fun CheckPermission(): Boolean {
-        if(
-            ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
-            ==PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
-            ==PackageManager.PERMISSION_GRANTED
-        ){
+
+
+        if (
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) ==
+            PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
             return true
+
         }
         return false
-
-
-    }
-
-    private fun LocationEnable(): Boolean {
-         var locationManager :LocationManager =getSystemService(Context.LOCATION_SERVICE ) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-
-
-
 
     }
 
     @SuppressLint("MissingPermission")
     private fun getLastLocation() {
-        if(CheckPermission()){
+        if (CheckPermission()) {
 
-            if(LocationEnable()) //gps off na ho
-                fusedLocationClient.lastLocation.addOnCompleteListener {
-                    task ->
-                    var location :Location = task.result
-                    if(location==null){
+            if (locationEnable()) {
+                fusedLocationClient.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
 
                         NewLocation()
+                    } else {
+                        Log.i("Location", location.longitude.toString())
 
-                    }else{
-                        Log.i("Location",location.longitude.toString())
                     }
+                }
 
-                }else{
-                    Toast.makeText(this,"Please Turn on your GPS Location",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Please turn on your GPS location", Toast.LENGTH_LONG).show()
+
             }
+        } else {
+            RequestPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun NewLocation() {
+        var locationRequest = LocationRequest()
+        locationRequest.priority = locationRequest.priority
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallBack,
+            Looper.myLooper()
+        )
 
 
-        }else{
-           RequestPermission()
+    }
 
-
+    private val locationCallBack = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLocation: Location? = p0.lastLocation
         }
 
     }
 
-    private fun NewLocation() {
+
+    private fun locationEnable(): Boolean {   // GPs on
+
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
 
     }
 
-
     private fun RequestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION ,
-            android.Manifest.permission.ACCESS_FINE_LOCATION),myRequestCode)
-
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ), myRequestCode
+        )
     }
 
     @SuppressLint("MissingSuperCall")
@@ -112,8 +133,8 @@ class SplashScreen : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode ==myRequestCode){
-            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == myRequestCode) {
+            if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getLastLocation()
             }
         }
